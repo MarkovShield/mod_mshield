871894734af5081a8d3383c0fa48ef337dd3ab25
Philip Schmid <philip.schmid@hsr.ch>
2017-03-29 16:50:35 +0200

    Renamed compile.sh in order to be more general

--- compile_mac.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make clean > /dev/null 2>&1
-/usr/local/Cellar/httpd24/2.4.25/bin/apxs -c -Wc,-O0 -Wc,-Wall -Wc,-DMOD_MSHIELD_SESSION_COUNT=10 -Wc,-DMOD_MSHIELD_COOKIESTORE_COUNT=30 mod_mshield.c
-make clean > /dev/null 2>&1
