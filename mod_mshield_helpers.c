35cd1229f9f3bed5abeaac5741b349a132d52a4b
Philip Schmid <philip.schmid@hsr.ch>
2017-04-11 23:02:54 +0200

    Fix version 1 for tailing / helper

--- mod_mshield_helpers.c
+++ mod_mshield_helpers.c
@@ -6,17 +6,15 @@
 const char *
 mshield_remove_trailing_slash(const char *arg1) {
 
-    char *temp = "";
-    if (arg1 != NULL) {
-        apr_cpystrn(temp, arg1, sizeof(temp));
-        size_t len = strlen(temp);
+    char *temp;
+    temp = malloc((strlen(arg1) + 1) * sizeof(char));
+    stpncpy(temp, arg1, sizeof(temp));
+    size_t len = strlen(temp);
 
-        if (len > 1) {
-            if ('/' == temp[len - 1]) {
-                temp[len - 1] = '\0';
-            }
+    if (len > 1) {
+        if ('/' == temp[len - 1]) {
+            temp[len - 1] = '\0';
         }
-
     }
 
     return temp;
