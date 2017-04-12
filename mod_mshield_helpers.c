039a2f5c9006cd810262e197253ad9271e359886
Philip Schmid <philip.schmid@hsr.ch>
2017-04-12 09:57:10 +0200

    Removed unused code

--- mod_mshield_helpers.c
+++ mod_mshield_helpers.c
@@ -7,10 +7,7 @@ const char *
 mshield_remove_trailing_slash(const char *arg1) {
 
     char *temp = (char *) arg1;
-    //temp = malloc((sizeof(arg1) + 1) * sizeof(char));
-    //apr_cpystrn(temp, arg1, sizeof(temp));
     size_t len = strlen(temp);
-    ap_log_error(PC_LOG_CRIT, NULL, "temp after stpncpy: [%s]", temp);
 
     if (len > 1) {
         if (temp[len - 1] == '/') {	
